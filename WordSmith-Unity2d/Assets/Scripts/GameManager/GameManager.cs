5dfb56ac2ad7653143dcf71d43fec24d1a740087
Adithya S <141550319+aDiThYa-808@users.noreply.github.com>
2025-02-28 01:00:59 +0530

    Added Levels List
    Added a new scene which contains 6 levels and a play button along with each level.

--- WordSmith-Unity2d/Assets/Scripts/GameManager/GameManager.cs
+++ WordSmith-Unity2d/Assets/Scripts/GameManager/GameManager.cs
@@ -1,23 +1,41 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; } // Singleton instance
 
-    
+    [Header("Loading")]
+    [SerializeField] private GameObject loadingScreen;
 
-    private void Awake()
+
+
+    public void Level1()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject); // Keeps it across scenes
-        }
-        else
-        {
-            Destroy(gameObject); // Prevents duplicates
-        }
+        loadscene("level-1");
     }
 
-   
+
+
+    void loadscene(string scenename)
+    {
+        StartCoroutine(loadSceneCoroutine(scenename));
+    }
+
+   IEnumerator loadSceneCoroutine(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+        loadingScreen.SetActive(true);
+
+        while(scene.progress < 0.9f)
+        {
+            yield return null;       
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        scene.allowSceneActivation = true;
+    }
 }
